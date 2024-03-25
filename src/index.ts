a2e635b8c554b205b8110e2a87fc60f7c115347a
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:15:10 +0100

    prod ready index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import App from './app';
 import PostController from '@/resources/post/post.controller';
 import UserController from '@/resources/user/user.controller';
 
+//our entry point, we are using dotenv a lib in TS to call our ENV file
+//we have the Post ontroller and the User controller also in our app and it is also listening to our app.ts
+//our PostController and UserController handles all the routes related to our post and user 
 validateEnv();
 
 const app = new App(
