e70a91f33236c9393b3de2a0273252309271b15e
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:14:18 +0100

    prod ready

--- src/resources/post/post.service.ts
+++ src/resources/post/post.service.ts
@@ -1,12 +1,12 @@
 import PostModel from '@/resources/post/post.model';
 import Post from '@/resources/post/post.interface';
 
+//this is where we call our mongodb queries that works togethr with our interface and model
 class PostService {
     private post = PostModel;
 
-    /**
-     * Create a new post
-     */
+   
+    //.create does put an entry to our db to create new product
     public async create(product: string, productPrice: number): Promise<Post> {
         try {
             const post = await this.post.create({  product, productPrice });
@@ -16,9 +16,7 @@ class PostService {
         }
     }
 
-    /**
-     * Get all posts
-     */
+    //.find get all the entries in our db
     public async getAllProducts(): Promise<Post[]> {
         try {
             const posts = await this.post.find();
@@ -28,9 +26,7 @@ class PostService {
         }
     }
 
-    /**
-     * Update a post by its ID
-     */
+    //.findOneAndUpdate helps us to find the latest one with the id nos and delete it
     public async update(id: number, product: string, productPrice: number): Promise<Post | null> {
         try {
             const updatedPost = await this.post.findOneAndUpdate({ id: id }, { product, productPrice }, { new: true });
@@ -40,9 +36,7 @@ class PostService {
         }
     }
 
-    /**
-     * Delete a post by its ID
-     */
+    //.findOneAndDelete works the same way post.findOneAndUpdate works, all this queries are provided by mongodb and they help to make accessing each collection easier for us
     public async delete(id: number): Promise<void> {
         try {
             await this.post.findOneAndDelete( {id: id} );
