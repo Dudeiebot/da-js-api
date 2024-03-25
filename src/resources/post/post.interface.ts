4497a11138d2122fc0b1eafe8eb3912706e53a19
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:14:50 +0100

    prod ready

--- src/resources/post/post.interface.ts
+++ src/resources/post/post.interface.ts
@@ -1,5 +1,6 @@
 import { Document, Schema, Types } from 'mongoose';
 
+ //defines the structure of documents that will be stored in the MongoDB collection associated with the Post model in the Mongoose schema and also do the type checking for us
 export default interface Post extends Document {
   id: Number;
   product: string;
