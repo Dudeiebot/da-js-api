9c27944b0c77102d8b923d833b667ec75e78248e
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:13:47 +0100

    prod ready

--- src/resources/user/user.interface.ts
+++ src/resources/user/user.interface.ts
@@ -1,5 +1,7 @@
 import { Document } from 'mongoose';
 
+
+ //defines the structure of documents that will be stored in the MongoDB collection associated with the Post model in the Mongoose schema and also do the type checking for us
 export default interface User extends Document {
     email: string;
     name: string;
