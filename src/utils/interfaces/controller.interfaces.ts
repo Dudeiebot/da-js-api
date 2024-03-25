94beb3184f5a0e8106a81591e7a6e41dce2efa9f
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:13:10 +0100

    prod ready

--- src/utils/interfaces/controller.interfaces.ts
+++ src/utils/interfaces/controller.interfaces.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+//handles the routes and path all round the Controller either User or Post
  export default interface Controller {
     path: string;
     router: Router;
