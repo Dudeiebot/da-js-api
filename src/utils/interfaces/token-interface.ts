13e9b0653dd713fb1f3eb84fe41b86246e92bbc0
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:12:42 +0100

    prod ready

--- src/utils/interfaces/token-interface.ts
+++ src/utils/interfaces/token-interface.ts
@@ -1,5 +1,6 @@
 import { Schema } from 'mongoose';
 
+//handles the schema in our mongodb for when our token token will expire with the required userId
 interface Token extends Object {
     id: Schema.Types.ObjectId;
     expiresIn: number;
