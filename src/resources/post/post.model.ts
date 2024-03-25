b8dc9e98ef45b0be01ebc0c09ae29a104eedb69c
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:14:31 +0100

    prod ready

--- src/resources/post/post.model.ts
+++ src/resources/post/post.model.ts
@@ -1,13 +1,20 @@
 import { Schema, model } from 'mongoose';
 import Post from '@/resources/post/post.interface';
 
+
+//our schema for the postProduct in mongodb is being defined here
+//unique is from mongodb and it is being added here to avoid duplicate entry creation from users
 const postSchema = new Schema<Post>({
     id: { type: Number, unique: true},
     product: { type: String, unique: true, index: true, required: true },
     productPrice: { type: Number, required: true }
 }, { timestamps: true });
 
-// Pre-save middleware to auto-increment id
+
+//so the thing about mongodb is that it creates each entry in our collection(mongodb tables)
+//with a objectId (the objectId is a set of strings), this objectId can be hard to get our delete and update works
+//so i implemented a id that is unique also and increments upon creation of new entry
+//this does the + and - for us
 postSchema.pre<Post>('save', async function (next) {
     if (!this.isNew) {
         return next();
