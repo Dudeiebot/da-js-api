8b7f232396a5a4e927d9a8b36eb405d6372ab6e1
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:14:11 +0100

    prod ready

--- src/resources/post/post.validation.ts
+++ src/resources/post/post.validation.ts
@@ -1,5 +1,6 @@
 import Joi from 'joi';
 
+//this is where our validation req.body is being checked and most importantly make the required also in our controller, equally type checking is done here also
 const create = Joi.object({
     product: Joi.string().required(),
     productPrice: Joi.number().required(),
