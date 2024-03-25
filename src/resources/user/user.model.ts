05a4a3c4661303321abaae8a4e164bfad9db3295
Dudeiebot <ojoexcel5@gmail.com>
2024-03-25 16:13:40 +0100

    prod ready

--- src/resources/user/user.model.ts
+++ src/resources/user/user.model.ts
@@ -2,6 +2,8 @@ import { Schema, model } from 'mongoose';
 import bcrypt from 'bcrypt';
 import User from '@/resources/user/user.interface';
 
+//our schema for the User in mongodb is being defined here
+//unique is from mongodb and it is being added here to avoid duplicate entry creation from users
 const UserSchema = new Schema(
     {
         name: {
@@ -25,29 +27,30 @@ const UserSchema = new Schema(
     { timestamps: true }
 );
 
-// Debugging console statement in the pre-save hook
 UserSchema.pre<User>('save', async function (next) {
-    console.log('Before saving user:', this);
+    // console.log('Before saving user:', this);
     
     if (!this.isModified('password')) {
-        console.log('Password not modified, skipping hash generation');
+        // console.log('Password not modified, skipping hash generation');
         return next();
     }
 
-    const hash = await bcrypt.hash(this.password, 10);
+    //we are using bcrypt to hash our password and we are telling bcrypt to hash the password in 10 rounds
+    const hashed = await bcrypt.hash(this.password, 10);
 
-    this.password = hash;
+    //we save our newly hashed password
+    this.password = hashed;
 
-    console.log('After hash generation:', this);
+    // console.log('After hash generation:', this);
     next();
 });
 
-// Debugging console statement in the isValidPassword method
+//check for validity of the password, when next the user login
 UserSchema.methods.isValidPassword = async function (
     password: string
 ): Promise<Error | boolean> {
-    console.log('Validating password:', password);
-    console.log('Stored hash:', this.password);
+    // console.log('Validating password:', password);
+    // console.log('Stored hash:', this.password);
     
     return await bcrypt.compare(password, this.password);
 };
